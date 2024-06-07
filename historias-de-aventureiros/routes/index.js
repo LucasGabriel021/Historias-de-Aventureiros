7680b820745ff7fcaea2134ba0dc08afb4e40a19
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-06-06 22:59:13 -0300

    Adicionando tela de Login e Cadastro

--- historias-de-aventureiros/routes/index.js
+++ historias-de-aventureiros/routes/index.js
@@ -5,9 +5,14 @@ const indexController = require("../controllers/indexController");
 
 router.get("/", indexController.index);
 
+// Rota para a página de login
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+});
+
 // Rota para a página de cadastro
-router.get('/entrarConta', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'entrarConta.html'));
+router.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'cadastro.html'));
 });
 
 module.exports = router;
