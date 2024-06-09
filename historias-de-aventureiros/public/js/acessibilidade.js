6716c3cd35f03aac3f38488138b733ec11472688
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-06-09 15:34:53 -0300

    Criando pasta models

--- historias-de-aventureiros/public/js/acessibilidade.js
+++ historias-de-aventureiros/public/js/acessibilidade.js
@@ -12,6 +12,20 @@ $(document).ready(function() {
                tentativaClicks++;
                console.log("Tentativas: ", tentativaClicks);
 
+               $.alert({
+                    title: "Aumentar fonte",
+                    content: "O tamanho da fonte foi aumentado em 10%.",
+                    autoClose: "alert|1000",
+                    buttons: {
+                         alert: {
+                           isHidden: true, 
+                           action: function () {
+                             // Ação do botão alert (pode ficar vazio)
+                           }
+                         }
+                       }
+               });
+
                $("h1, h2, h3, h4, h5, p, span, button, li, a").each(function() {
                     const tamanhoAtual = parseFloat($(this).css("font-size"));
                     // console.log(tamanhoAtual);
@@ -24,6 +38,21 @@ $(document).ready(function() {
      btnDiminuirFonte.click(function() {
           if(tentativaClicks > 0) {
                tentativaClicks--;
+
+               $.alert({
+                    title: "Diminuir fonte",
+                    content: "O tamanho da fonte foi diminuído em 10%.",
+                    autoClose: "alert|1000",
+                    buttons: {
+                         alert: {
+                           isHidden: true, 
+                           action: function () {
+                             // Ação do botão alert (pode ficar vazio)
+                           }
+                         }
+                       }
+               });
+
                $("h1, h2, h3, h4, h5, p, span, button, li, a").each(function() {
                     const tamanhoAtual = parseFloat($(this).css("font-size"));
                     const novoTamanhoFonte = tamanhoAtual * decrementarValor;
