c796e64cfe18912f25de6017ad4d0bd4e999cdc6
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-06-14 21:49:13 -0300

    Corrigindo bug de Acessibilidade Protanopia e Deuteranopia

--- historias-de-aventureiros/public/js/acessibilidade.js
+++ historias-de-aventureiros/public/js/acessibilidade.js
@@ -64,6 +64,7 @@ $(document).ready(function() {
 
      const textoVerde = $(".text-verde-primario");
      const textoEscuroVerde = $(".hover\\:text-verde-escuro");
+     const textoHoverVerde = $(".hover\\:text-verde-primario");
      const bgVerde = $(".bg-verde-primario");
      const bgSecundarioVerde = $(".bg-verde-secundario");
      const bgClaroVerde = $(".bg-verde-claro");
@@ -79,33 +80,49 @@ $(document).ready(function() {
           switch(corSelecionada) {
                case "Normal":
                     console.log("Normal");
-                    corProtanopia(textoVerde, "text-verde-primario-protanopia", "text-verde-primario");
-                    corProtanopia(textoEscuroVerde, "hover:text-verde-escuro-protanopia", "hover:text-verde-escuro");
-                    corProtanopia(bgVerde, "bg-verde-primario-protanopia", "bg-verde-primario");
-                    corProtanopia(bgSecundarioVerde, "bg-verde-secundario-protanopia", "bg-verde-secundario");
-                    corProtanopia(bgClaroVerde, "bg-verde-claro-protanopia", "bg-verde-claro");
-                    corProtanopia(bgVerdeEscuroHover, "hover:bg-verde-escuro-protanopia", "hover:bg-verde-escuro");
-                    corProtanopia(bgVerdeSecundarioHover, "hover:bg-verde-secundario-protanopia", "hover:bg-verde-secundario");
-                    corProtanopia(tagImg, "filter-protanopia", "");
-                    corProtanopia(styleImg, "filter-protanopia", "");
+                    trocarCor(textoVerde, "text-verde-primario-deuteranopia text-verde-primario-protanopia", "text-verde-primario");
+                    trocarCor(textoEscuroVerde, "hover:text-verde-escuro-deuteranopia hover:text-verde-escuro-protanopia", "hover:text-verde-escuro");
+                    trocarCor(textoHoverVerde, "hover:text-verde-primario-deuteranopia hover:text-verde-primario-protanopia", "hover:text-verde-primario");
+                    trocarCor(bgVerde, "bg-verde-primario-deuteranopia bg-verde-primario-protanopia", "bg-verde-primario");
+                    trocarCor(bgSecundarioVerde, "bg-verde-secundario-deuteranopia bg-verde-secundario-protanopia", "bg-verde-secundario");
+                    trocarCor(bgClaroVerde, "bg-verde-claro-deuteranopia bg-verde-claro-protanopia", "bg-verde-claro");
+                    trocarCor(bgVerdeEscuroHover, "hover:bg-verde-escuro-deuteranopia hover:bg-verde-escuro-protanopia", "hover:bg-verde-escuro");
+                    trocarCor(bgVerdeSecundarioHover, "hover:bg-verde-secundario-deuteranopia hover:bg-verde-secundario-protanopia", "hover:bg-verde-secundario");
+                    trocarCor(tagImg, "filter-deuteranopia filter-protanopia", "");
+                    trocarCor(styleImg, "filter-deuteranopia filter-protanopia", "");
                     break;
                case "Protanopia":
                     console.log("Protanopia");
-                    corProtanopia(textoVerde, "text-verde-primario", "text-verde-primario-protanopia");
-                    corProtanopia(textoEscuroVerde, "hover:text-verde-escuro", "hover:text-verde-escuro-protanopia");
-                    corProtanopia(bgVerde, "bg-verde-primario", "bg-verde-primario-protanopia");
-                    corProtanopia(bgSecundarioVerde, "bg-verde-secundario", "bg-verde-secundario-protanopia");
-                    corProtanopia(bgClaroVerde, "bg-verde-claro", "bg-verde-claro-protanopia");
-                    corProtanopia(bgVerdeEscuroHover, "hover:bg-verde-escuro", "hover:bg-verde-escuro-protanopia");
-                    corProtanopia(bgVerdeSecundarioHover, "hover:bg-verde-secundario", "hover:bg-verde-secundario-protanopia");
-                    corProtanopia(tagImg, "", "filter-protanopia");
-                    corProtanopia(styleImg, "", "filter-protanopia");
+                    trocarCor(textoVerde, "text-verde-primario", "text-verde-primario-protanopia");
+                    trocarCor(textoEscuroVerde, "hover:text-verde-escuro", "hover:text-verde-escuro-protanopia");
+                    trocarCor(textoHoverVerde, "hover:text-verde-primario", "hover:text-verde-primario-protanopia");
+                    trocarCor(bgVerde, "bg-verde-primario", "bg-verde-primario-protanopia");
+                    trocarCor(bgSecundarioVerde, "bg-verde-secundario", "bg-verde-secundario-protanopia");
+                    trocarCor(bgClaroVerde, "bg-verde-claro", "bg-verde-claro-protanopia");
+                    trocarCor(bgVerdeEscuroHover, "hover:bg-verde-escuro", "hover:bg-verde-escuro-protanopia");
+                    trocarCor(bgVerdeSecundarioHover, "hover:bg-verde-secundario", "hover:bg-verde-secundario-protanopia");
+                    trocarCor(tagImg, "", "filter-protanopia");
+                    trocarCor(styleImg, "", "filter-protanopia");
+                    
+                    break;
+               case "Deuteranopia":
+                    console.log("Deuteranopia");
+                    trocarCor(textoVerde, "text-verde-primario", "text-verde-primario-deuteranopia");
+                    trocarCor(textoEscuroVerde, "hover:text-verde-escuro", "hover:text-verde-escuro-deuteranopia");
+                    trocarCor(textoHoverVerde, "hover:text-verde-primario", "hover:text-verde-primario-deuteranopia");
+                    trocarCor(bgVerde, "bg-verde-primario", "bg-verde-primario-deuteranopia");
+                    trocarCor(bgSecundarioVerde, "bg-verde-secundario", "bg-verde-secundario-deuteranopia");
+                    trocarCor(bgClaroVerde, "bg-verde-claro", "bg-verde-claro-deuteranopia");
+                    trocarCor(bgVerdeEscuroHover, "hover:bg-verde-escuro", "hover:bg-verde-escuro-deuteranopia");
+                    trocarCor(bgVerdeSecundarioHover, "hover:bg-verde-secundario", "hover:bg-verde-secundario-deuteranopia");
+                    trocarCor(tagImg, "", "filter-deuteranopia");
+                    trocarCor(styleImg, "", "filter-deuteranopia");
                     break;
      
           }
      })
 
-     function corProtanopia(seletor, classeAntiga, classeNova) {
+     function trocarCor(seletor, classeAntiga, classeNova) {
           $(seletor).each(function() {
                $(this).removeClass(classeAntiga).addClass(classeNova);
           });
